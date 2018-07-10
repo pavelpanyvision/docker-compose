957d4d9ebd62ec5b7e830b42023996d527d6fa1a
vadim-so <40731013+vadim-so@users.noreply.github.com>
2018-07-10 12:24:55 +0300

    Update and rename checkmongo.sh to mongo_migration_1.19.2.sh

--- migrations/mongo_migration_1.19.2.sh
+++ migrations/mongo_migration_1.19.2.sh
@@ -13,12 +13,14 @@ display_usage() {
 
 #Params
 #SCRIPT_DIR="Dash-API/scripts"
-SCRIPT_DIR=$1
-SCRIPT_NAME=$(echo $SCRIPT_DIR| awk -F/ '{print $NF}')
-MONGO_IMAGE="gcr.io/anyvision-training/mongo:3.6-jessie"
-HOSTNAME=127.0.0.1
+SCRIPT=$1
+#SCRIPT_NAME=$(echo $SCRIPT_DIR| awk -F/ '{print $NF}')
+#MONGO_IMAGE="gcr.io/anyvision-training/mongo:3.6-jessie"
+#HOSTNAME=127.0.0.1
+HOSTNAME=mongo
 PORT=27017
 echo "Starting the Mongo Container Image and change its name to Mongo"
+#docker run -d -v /var/lib/docker/volumes/anyvision_backend_data --name=mongo $MONGO_IMAGE
 
 #echo "Copy $SCRIPT_DIR/$SCRIPT into /tmp/ inside the container"
 echo "Copy $SCRIPT_DIR into /tmp/ inside the container as $SCRIPT_DIR"
@@ -27,9 +29,10 @@ if [[ ! -f $SCRIPT_DIR ]]
 then
         echo "$SCRIPT_DIR not found!"
 else
-        docker cp $SCRIPT_DIR docker-compose_mongodb_1:/tmp/$SCRIPT_NAME
+        #docker cp $SCRIPT_DIR docker-compose_mongodb_1:/tmp/$SCRIPT_NAME
         echo "Executing Mongo anyVision2 $SCRIPT_NAME"
-        docker exec docker-compose_mongodb_1 bash -c "mongo localhost:27017/anyVision2 /tmp/$SCRIPT_NAME"
+        #docker exec docker-compose_mongodb_1 bash -c "mongo localhost:27017/anyVision2 /tmp/$SCRIPT_NAME"
+        mongo $HOSTNAME:$PORT/anyVision2 $SCRIPT_DIR
         #docker exec mongo bash -c "mongo localhost:27017/anyVision2 /tmp/$SCRIPT_NAME"
 fi
 #echo "Stopping and Removing Mongo container for reuse"
