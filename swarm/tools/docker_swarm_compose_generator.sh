36d73b4b023c5b3af9bf231dc7ff968648212fdf
Ziv <digger18@gmail.com>
2018-07-12 00:57:32 +0300

    Updated Swarm script, added air-gap installation method for meta-compose, generated new stacks

--- swarm/tools/docker_swarm_compose_generator.sh
+++ swarm/tools/docker_swarm_compose_generator.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -eu
 
-# Requirements:
+# Requires https://github.com/webcrofting/meta-compose:
+# Online install:
 # pip install meta-compose
 
-# From https://github.com/webcrofting/meta-compose
+# Offline (air-gapped) install:
+# pip install --no-index --find-links="./meta-compose" meta-compose
 
 mkdir -p sites/
 
@@ -12,7 +14,9 @@ while IFS='' read -r site || [[ -n "$site" ]]; do
     SITE_NAME=$site
     echo "Generating Docker Compose file for $SITE_NAME"
     export SITE_NAME="$SITE_NAME"
-    meta-compose -t docker-compose-swarm-gpu.yml.tmpl -o sites/docker-compose-$SITE_NAME.yml
+    mkdir -p sites/$SITE_NAME
+    /usr/local/bin/meta-compose -t docker-compose-swarm-gpu.yml.tmpl -o sites/$SITE_NAME/docker-compose-$SITE_NAME.yml
+    cp -R ../../env ../../guacamole sites/$SITE_NAME/
 done < "sites.txt"
 
 echo "Done!"
