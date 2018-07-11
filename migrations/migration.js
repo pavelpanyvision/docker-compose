f58e31edf106c7d34b818e26662aab7cab7e477d
root <you@example.com>
2018-07-11 16:48:05 +0300

    add handling images to the migration script

--- migrations/migration.js
+++ migrations/migration.js
@@ -12,3 +12,14 @@ while (cursor.hasNext()){
 db.suspects.remove({ignore: true})
 db.detections.remove({label: 'ignored'})
 db.suspect_groups.update({_id: ObjectId("000000000000000000000002")}, { $set:{title:"Low Quality Images"}}
+
+detectionsCursor = db.detections.find();
+while (detectionsCursor.hasNext()) {
+  detection = detectionsCursor.next();
+  if (!detection.images) continue;
+
+  detection.zoom_in_images = detection.images.filter(function(i){return !i.includes('Large');});
+  detection.zoom_out_images = detection.images.filter(function(i){return i.includes('Large');});
+  delete detection.images;
+  db.detections.save(detection);
+}
