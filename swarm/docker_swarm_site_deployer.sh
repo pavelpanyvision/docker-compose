3bff6d272243134191b0fac42847aeee930602c0
root <ziv@anyvision.co>
2018-08-03 05:41:33 +0300

    Added docker_swarm_site_deployer.sh

---
+++ swarm/docker_swarm_site_deployer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -eu
+
+
+docker_warning() {
+  echo "This script must be run with Docker capable privileges!"
+}
+
+
+# Check Docker command executable exit code
+docker images > /dev/null 2>&1; rc=$?;
+if [[ $rc != 0 ]]; then
+  docker_warning
+  exit 1
+fi
+
+
+## Remove and Deploy stack for each site
+while IFS='' read -r site || [[ -n "$site" ]]; do
+    SITE_NAME="$site"
+    echo "Removing stack \"$SITE_NAME\""
+    docker stack rm "$SITE_NAME"
+    echo "Deploying stack \"$SITE_NAME\" using file sites/$SITE_NAME/docker-compose-$SITE_NAME.yml"
+    docker stack deploy -c sites/"$SITE_NAME"/docker-compose-"$SITE_NAME".yml "$SITE_NAME"
+done < "sites.txt"
+
+
+echo "Done!"
+exit 0
