413e08ef09e9174c4622232a0d8825740eb9cb79
root <ziv@anyvision.co>
2018-08-07 12:33:15 +0300

    Added TZ and run-compose.sh script, remove /etc/localtime and timezone for RHEL compatability

---
+++ run-compose.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+# Absolute path to this script
+SCRIPT=$(readlink -f "$0")
+# Absolute path to the script directory
+BASEDIR=$(dirname "$SCRIPT")
+
+export TZ=`readlink /etc/localtime | awk -F / '{print $5"/"$6}'`
+
+docker-compose -f "$BASEDIR"/"$1" -p "$2" ${*:3}
