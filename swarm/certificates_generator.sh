d0b89183cc6503e33cac3d562b1df444c0cb3469
root <root@qa-team-d-europe-west1-d-swarm-manager-1.c.anyvision-training.internal>
2018-08-07 05:09:14 +0000

    Moved anyvisionCA.cnf under templates/

--- swarm/certificates_generator.sh
+++ swarm/certificates_generator.sh
@@ -13,7 +13,7 @@ create_ssl() {
 	
 	echo " Generate self-signed CA certificate signed by our own CA using the config we editted earlier."
 	# -x509 means public key, -subj means cert issuer (SAN), crucial for the validity of the cert.
-	openssl req -new -x509 -subj "/CN=anyvisionCA" -extensions v3_ca -days 3650 -key $CA_CERTS_DIR/anyvisionCA.key.pem -sha256 -out $CA_CERTS_DIR/anyvisionCA.pem -config tls/anyvisionCA.cnf -passin pass:$password
+	openssl req -new -x509 -subj "/CN=anyvisionCA" -extensions v3_ca -days 3650 -key $CA_CERTS_DIR/anyvisionCA.key.pem -sha256 -out $CA_CERTS_DIR/anyvisionCA.pem -config templates/anyvisionCA.cnf -passin pass:$password
 	
 	echo " Generate private/public RSA key pair"
 	# We won't use a passphrase here. (just not necessary)
@@ -26,7 +26,7 @@ create_ssl() {
 	openssl req -subj "/CN=apigateway.anyvision.local" -extensions v3_req -sha256 -new -key $CA_CERTS_DIR/apigateway.anyvision.local.key.pem -out $CA_CERTS_DIR/apigateway.anyvision.local.csr
 	
 	echo "Generate a signed(by our own CA) certificate for our host"
-	openssl x509 -req -extensions v3_req -days 3650 -sha256 -passin pass:$password -in $CA_CERTS_DIR/apigateway.anyvision.local.csr -CA $CA_CERTS_DIR/anyvisionCA.pem -CAkey $CA_CERTS_DIR/anyvisionCA.key.pem -CAcreateserial -out $CA_CERTS_DIR/apigateway.anyvision.local.crt -extfile tls/anyvisionCA.cnf
+	openssl x509 -req -extensions v3_req -days 3650 -sha256 -passin pass:$password -in $CA_CERTS_DIR/apigateway.anyvision.local.csr -CA $CA_CERTS_DIR/anyvisionCA.pem -CAkey $CA_CERTS_DIR/anyvisionCA.key.pem -CAcreateserial -out $CA_CERTS_DIR/apigateway.anyvision.local.crt -extfile templates/anyvisionCA.cnf
 	
 	cd $CA_CERTS_DIR
 	cat apigateway.anyvision.local.crt  anyvisionCA.pem anyvisionCA.key.pem > apigateway.anyvision.local.full.pem
