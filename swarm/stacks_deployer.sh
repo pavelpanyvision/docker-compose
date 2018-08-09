c89377ffb13948d8321418de69f3efccbdf552f3
root <root@qa-team-d-europe-west1-d-swarm-manager-1.c.anyvision-training.internal>
2018-08-09 02:52:37 +0000

    Rollback to nginx proxy configuration, updated stack generator and deployer scripts.

--- swarm/stacks_deployer.sh
+++ swarm/stacks_deployer.sh
@@ -5,6 +5,10 @@ set -eu
 # Source the optparse.bash file ---------------------------------------------------
 source optparse.bash
 # Define options
+optparse.define short=s long=sites desc="Deploy stacks from sites.txt" variable=deploy_sites value=true default=false
+optparse.define short=m long=management desc="Deploy management stack" variable=deploy_management value=true default=false
+optparse.define short=i long=apimaster desc="Deploy api-master stack" variable=deploy_apimaster value=true default=false
+optparse.define short=a long=all desc="Deploy all stacks" variable=deploy_all value=true default=false
 optparse.define short=r long=remove desc="Remove stack before deploy" variable=remove value=true default=false
 # Source the output file ----------------------------------------------------------
 source $( optparse.build )
@@ -23,6 +27,12 @@ if [[ $rc != 0 ]]; then
 fi
 
 
+if [ "$deploy_sites" = "false" ] && [ "$deploy_all" = "false" ] && [ "$deploy_apimaster" = "false" ] && [ "$deploy_management" = "false" ]; then
+  echo "Error: Nothing to deploy, please choose which stacks you wish to deploy."
+  exit 1
+fi
+
+
 # Absolute path to this script
 SCRIPT=$(readlink -f "$0")
 # Absolute path to the script directory
@@ -30,36 +40,42 @@ BASEDIR=$(dirname "$SCRIPT")
 
 
 ## Remove and Deploy stack for each site
-while IFS='' read -r site || [[ -n "$site" ]]; do
-    SITE_NAME="$site"
-    if [ "$remove" = "true" ]; then
-      echo "Removing stack \"$SITE_NAME\""
-      docker stack rm "$SITE_NAME"
-      sleep 15
-    fi
-    echo "Deploying stack \"$SITE_NAME\" using file stacks/$SITE_NAME/docker-stack-$SITE_NAME.yml"
-    docker stack deploy --with-registry-auth --resolve-image always -c "$BASEDIR"/stacks/"$SITE_NAME"/docker-stack-"$SITE_NAME".yml "$SITE_NAME"
-done < "$BASEDIR"/sites.txt
+if [ "$deploy_sites" = "true" ] || [ "$deploy_all" = "true" ]; then
+  while IFS='' read -r site || [[ -n "$site" ]]; do
+      SITE_NAME="$site"
+      if [ "$remove" = "true" ]; then
+        echo "Removing stack \"$SITE_NAME\""
+        docker stack rm "$SITE_NAME"
+        sleep 15
+      fi
+      echo "Deploying stack \"$SITE_NAME\" using file stacks/$SITE_NAME/docker-stack-$SITE_NAME.yml"
+      docker stack deploy --with-registry-auth --resolve-image always -c "$BASEDIR"/stacks/"$SITE_NAME"/docker-stack-"$SITE_NAME".yml "$SITE_NAME"
+  done < "$BASEDIR"/sites.txt
+fi
 
 
 ## API Master stack
-if [ "$remove" = "true" ]; then
-  echo "Removing stack \"api-master\""
-  docker stack rm api-master
-  sleep 15
+if [ "$deploy_apimaster" = "true" ] || [ "$deploy_all" = "true" ]; then
+  if [ "$remove" = "true" ]; then
+    echo "Removing stack \"api-master\""
+    docker stack rm api-master
+    sleep 15
+  fi
+  echo "Deploying stack \"api-master\" using file stacks/api-master/docker-stack-api-master.yml"
+  docker stack deploy --with-registry-auth --resolve-image always -c "$BASEDIR"/stacks/api-master/docker-stack-api-master.yml api-master
 fi
-echo "Deploying stack \"api-master\" using file stacks/api-master/docker-stack-api-master.yml"
-docker stack deploy --with-registry-auth --resolve-image always -c "$BASEDIR"/stacks/api-master/docker-stack-api-master.yml api-master
 
 
 ## Management stack
-if [ "$remove" = "true" ]; then
-  echo "Removing stack \"management\""
-  docker stack rm management
-  sleep 15
+if [ "$deploy_management" = "true" ] || [ "$deploy_all" = "true" ]; then
+  if [ "$remove" = "true" ]; then
+    echo "Removing stack \"management\""
+    docker stack rm management
+    sleep 15
+  fi
+  echo "Deploying stack \"management\" using file stacks/management/docker-stack-management.yml"
+  docker stack deploy --with-registry-auth --resolve-image always -c "$BASEDIR"/stacks/management/docker-stack-management.yml management
 fi
-echo "Deploying stack \"management\" using file stacks/management/docker-stack-management.yml"
-docker stack deploy --with-registry-auth --resolve-image always -c "$BASEDIR"/stacks/management/docker-stack-management.yml management
 
 
 echo "Done!"
--- swarm/stacks_generator.sh
+++ swarm/stacks_generator.sh
@@ -11,12 +11,13 @@
 # Source the optparse.bash file ---------------------------------------------------
 source optparse.bash
 # Define options
-optparse.define short=s long=sites desc="Generate stacks from sites.txt" variable=generate_sites value=true default=true
+optparse.define short=s long=sites desc="Generate stacks from sites.txt" variable=generate_sites value=true default=false
 optparse.define short=b long=ab desc="Generate A/B stacks from sites.txt" variable=generate_ab value=true default=false
-optparse.define short=m long=management desc="Generate management stack" variable=generate_management value=true default=true
-optparse.define short=a long=apimaster desc="Generate api-master stack" variable=generate_apimaster value=true default=true
-optparse.define short=r long=registry desc="Registry URI, for example: \"gcr.io/anyvision-training\" [required]" variable=registry
-optparse.define short=p long=port desc="Registry Port, for example: \":5000\" [optional]" variable=port default=""
+optparse.define short=m long=management desc="Generate management stack" variable=generate_management value=true default=false
+optparse.define short=i long=apimaster desc="Generate api-master stack" variable=generate_apimaster value=true default=false
+optparse.define short=a long=all desc="Generate all stacks" variable=generate_all value=true default=false
+optparse.define short=r long=registry desc="Registry URI, for example: \"gcr.io/anyvision-production\" or \"registry.anyvision.local:5000\" [required]" variable=registry
+#optparse.define short=p long=port desc="Registry Port, for example: \":5000\" [optional]" variable=port default=""
 # Source the output file ----------------------------------------------------------
 source $( optparse.build )
 
@@ -26,13 +27,18 @@ docker_warning() {
 }
 
 
+if [ "$generate_sites" = "false" ] && [ "$generate_all" = "false" ] && [ "$generate_apimaster" = "false" ] && [ "$generate_management" = "false" ]; then
+  echo "Error: Nothing to generate, please choose which stacks you wish to generate."
+  exit 1
+fi
+
+
 # Check Docker command executable exit code
 docker images > /dev/null 2>&1; rc=$?;
 if [[ $rc != 0 ]]; then
   docker_warning
   exit 1
 fi
-
 
 if [ -z "$registry" ]; then
   echo "Error: Registry URI must be provided."
@@ -53,33 +59,34 @@ SCRIPT=$(readlink -f "$0")
 # Absolute path to the script directory
 BASEDIR=$(dirname "$SCRIPT")
 
-rm -rf "$BASEDIR"/stacks/
-mkdir -p "$BASEDIR"/stacks/
-
 
 ## Generate unique stack for each site
-while IFS='' read -r site || [[ -n "$site" ]]; do
+if [ "$generate_sites" = "true" ] || [ "$generate_all" = "true" ]; then
+  while IFS='' read -r site || [[ -n "$site" ]]; do
     SITE_NAME="$site"
-    echo "Generating Docker stack file for $SITE_NAME"
     export SITE_NAME="$SITE_NAME"
+    rm -rf "$BASEDIR"/stacks/"$SITE_NAME"
     mkdir -p "$BASEDIR"/stacks/"$SITE_NAME"
-    if [ "$generate_sites" = "true" ] && [ "$generate_ab" = "false" ] ; then
-        /usr/local/bin/meta-compose -t templates/node-gpu-stack.yml.tmpl -o "$BASEDIR"/stacks/"$SITE_NAME"/docker-stack-"$SITE_NAME".yml
+    if [ "$generate_ab" = "false" ] ; then
+      echo "Generating Docker stack file for \"$SITE_NAME\""
+      /usr/local/bin/meta-compose -t templates/node-gpu-stack.yml.tmpl -o "$BASEDIR"/stacks/"$SITE_NAME"/docker-stack-"$SITE_NAME".yml
     else
-        /usr/local/bin/meta-compose -t templates/node-gpu-stack-a.yml.tmpl -o "$BASEDIR"/stacks/"$SITE_NAME"/docker-stack-"$SITE_NAME".yml
+      echo "Generating Docker stack file for \"$SITE_NAME\" as 'A' site"
+      /usr/local/bin/meta-compose -t templates/node-gpu-stack-a.yml.tmpl -o "$BASEDIR"/stacks/"$SITE_NAME"/docker-stack-"$SITE_NAME".yml
     fi
     cp -R "$BASEDIR"/../{env,crontab,guacamole} --target-directory="$BASEDIR"/stacks/"$SITE_NAME"/
     ln -s "$BASEDIR"/tls "$BASEDIR"/stacks/"$SITE_NAME"/tls
     rm "$BASEDIR"/stacks/"$SITE_NAME"/guacamole/user-mapping-local.xml
     sed -i 's/>desktop</>desktop-'"$SITE_NAME"'\.anyvision\.local</g' "$BASEDIR"/stacks/"$SITE_NAME"/guacamole/user-mapping-cloud.xml
     sed -i 's/>sftp</>sftp-'"$SITE_NAME"'\.anyvision\.local</g' "$BASEDIR"/stacks/"$SITE_NAME"/guacamole/user-mapping-cloud.xml
-done < "$BASEDIR"/sites.txt
-
+  done < "$BASEDIR"/sites.txt
+fi
 
 
 ## Generate the management stack
-if [ "$generate_management" = "true" ] ; then
+if [ "$generate_management" = "true" ] || [ "$generate_all" = "true" ]; then
     echo "Generating Docker Management stack file"
+    rm -rf "$BASEDIR"/stacks/management
     mkdir -p "$BASEDIR"/stacks/management
     cp -R "$BASEDIR"/../crontab --target-directory="$BASEDIR"/stacks/management/
     ln -s "$BASEDIR"/tls "$BASEDIR"/stacks/management/tls
@@ -87,8 +94,9 @@ if [ "$generate_management" = "true" ] ; then
 fi
 
 ## Generate the api-master stack
-if [ "$generate_apimaster" = "true" ] ; then
+if [ "$generate_apimaster" = "true" ] || [ "$generate_all" = "true" ]; then
     echo "Generating Docker API-Master stack file"
+    rm -rf "$BASEDIR"/stacks/api-master
     mkdir -p "$BASEDIR"/stacks/api-master
     cp -R "$BASEDIR"/../env --target-directory="$BASEDIR"/stacks/api-master/
     ln -s "$BASEDIR"/tls "$BASEDIR"/stacks/api-master/tls
@@ -97,9 +105,10 @@ if [ "$generate_apimaster" = "true" ] ; then
 fi
 
 ## Generate the ab stack
-if [ "$generate_ab" = "true" ] ; then
+if [ "$generate_ab" = "true" ]; then
     echo "Generating Docker \"B\" stack file"
     export SITE_NAME="b"
+    rm -rf "$BASEDIR"/stacks/"$SITE_NAME"
     mkdir -p "$BASEDIR"/stacks/"$SITE_NAME"
     cp -R "$BASEDIR"/../env --target-directory="$BASEDIR"/stacks/"$SITE_NAME"/
     ln -s "$BASEDIR"/tls "$BASEDIR"/stacks/"$SITE_NAME"/tls
