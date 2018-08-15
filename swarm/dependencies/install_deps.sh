4259aca211dd29d6c4ed1dbec93b43a30fa93fc2
root <ziv@anyvision.co>
2018-08-15 18:50:52 +0300

    Added support and scripts for CoreDNS (Untested)

---
+++ swarm/dependencies/install_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+pip install --no-index --find-links ./meta-compose meta-compose
+pip install --no-index --find-links ./j2cli j2cli
---
+++ swarm/dns_generator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Absolute path to this script
+SCRIPT=$(readlink -f "$0")
+# Absolute path to the script directory
+BASEDIR=$(dirname "$SCRIPT")
+
+mkdir -p "$BASEDIR"/stacks/management/coredns
+
+j2 ./templates/coredns-zonefile.tmpl sites.yml > "$BASEDIR"/stacks/management/coredns/db.zonefile
+j2 ./templates/coredns-Corefile.tmpl sites.yml > "$BASEDIR"/stacks/management/coredns/Corefile
+
+echo 'Done!'
+exit 0
