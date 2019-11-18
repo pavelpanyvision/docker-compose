57eb74ffd3f937e55dc0281afdc588c002aca1fb
OriBenHur-Any <orib@anyvision.co>
2019-11-18 21:06:13 +0200

    Moved all logic from main to separate methods

--- change_mode.py
+++ change_mode.py
@@ -9,6 +9,27 @@ import argparse
 import math
 
 
+def check_root():
+    euid = os.geteuid()
+    if euid != 0:
+        raise EnvironmentError('need to be root')
+
+
+def get_params():
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-m', '--mode', help='set mode', action='store', choices=['unicast', 'multicast'], dest='MODE')
+    group.add_argument('--get-mode', help='get current mode', action='store_true', dest='getMode')
+    args = parser.parse_args()
+    if args.getMode:
+        print(getmode())
+        exit(0)
+    if args.MODE == getmode():
+        print('you are already on {}, aborting'.format(args.MODE))
+        exit(0)
+    return args.MODE
+
+
 def get_ip_address(dev):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
@@ -59,6 +80,7 @@ def set_mode(mode):
     mode_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), '.MODE')
     with open(mode_file, 'w+') as f:
         f.write(mode)
+    print('Mode is now: {}'.format(mode))
 
 
 def is_contains(pattern, file_path):
@@ -84,61 +106,61 @@ def update_hosts(path, line, present=True):
                 f.write("{}\n".format(item))
 
 
-def main():
-    euid = os.geteuid()
-    if euid != 0:
-        raise EnvironmentError('need to be root')
-    yml_file = os.path.join(os.getcwd(), 'docker-compose-gpu.yml')
+def multicast(st, ip):
+    add_entries(st, 'extra_hosts', 'edge.tls.ai:{}'.format(ip), ['api', 'backend', 'collate'])
+    add_entries(st, 'network_mode', 'host', ['edge'])
+    add_entries(st, 'dns', '127.0.0.53', ['edge'])
+    add_entries(st, 'volumes', '/etc/hosts:/etc/hosts', ['edge'])
+    add_entries(st, 'extra_hosts', 'edge.tls.ai:127.0.0.1', ['edge'])
+    remove_entries(st, 'volumes', '/tmp/pipe_data:/root/pipe_data', ['backend', 'collate'])
+    add_entries(st, 'volumes', '/ssd/pipe_data:/root/pipe_data', ['backend', 'collate'])
+    remove_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=true', ['edge'])
+    add_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=false', ['edge'])
+    remove_keys(st, 'networks', ['edge'])
+    remove_keys(st, 'ports', ['edge'])
+    update_hosts('/etc/hosts', '127.0.0.1 edge.tls.ai')
+
+
+def unicast(st, ip):
+    add_entries(st, 'networks', {'prod': {'aliases': ('edge.tls.ai', 'proc-localnode.tls.ai')}}, ['edge'])
+    add_entries(st, 'ports', '4005:4005', ['edge'])
+    remove_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=false', ['edge'])
+    add_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=true', ['edge'])
+    remove_entries(st, 'volumes', '/ssd/pipe_data:/root/pipe_data', ['backend', 'collate'])
+    add_entries(st, 'volumes', '/tmp/pipe_data:/root/pipe_data', ['backend', 'collate'])
+    remove_entries(st, 'extra_hosts', 'edge.tls.ai:127.0.0.1', ['edge'])
+    remove_entries(st, 'volumes', '/etc/hosts:/etc/hosts', ['edge'])
+    remove_keys(st, 'dns', ['edge'])
+    remove_keys(st, 'network_mode', ['edge'])
+    remove_entries(st, 'extra_hosts', 'edge.tls.ai:{}'.format(ip), ['backend', 'collate'])
+    remove_keys(st, 'extra_hosts', ['api'])
+    update_hosts('/etc/hosts', '127.0.0.1 edge.tls.ai', False)
+
+
+def is_exist(file):
+    yml_file = os.path.join(os.getcwd(), file)
     if not os.path.exists(yml_file):
         raise EnvironmentError('No docker-compose-gpu.yml in here please make sure it exist in the working dir')
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-m', '--mode', help='set mode', action='store', choices=['unicast', 'multicast'], dest='MODE')
-    group.add_argument('--get-mode', help='get current mode', action='store_true', dest='getMode')
-    args = parser.parse_args()
-    if args.getMode:
-        print(getmode())
-        exit(0)
+    return yml_file
+
+
+def main():
+    check_root()
+    mode = get_params()
+    yml_file = is_exist('docker-compose-gpu.yml')
+    edge_ip = get_ip_address('docker0')
+
     with open(yml_file, 'r') as f:
         st = yaml.round_trip_load(f, preserve_quotes=True)
-    if args.MODE == getmode():
-        print('you are already on {}, aborting'.format(args.MODE))
-        exit(0)
-    mode = args.MODE
-    edge_ip = get_ip_address('docker0')
+
     if mode == 'multicast':
-        add_entries(st, 'extra_hosts', 'edge.tls.ai:{}'.format(edge_ip), ['api', 'backend', 'collate'])
-        add_entries(st, 'network_mode', 'host', ['edge'])
-        add_entries(st, 'dns', '127.0.0.53', ['edge'])
-        add_entries(st, 'volumes', '/etc/hosts:/etc/hosts', ['edge'])
-        add_entries(st, 'extra_hosts', 'edge.tls.ai:127.0.0.1', ['edge'])
-        remove_entries(st, 'volumes', '/tmp/pipe_data:/root/pipe_data', ['backend', 'collate'])
-        add_entries(st, 'volumes', '/ssd/pipe_data:/root/pipe_data', ['backend', 'collate'])
-        remove_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=true', ['edge'])
-        add_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=false', ['edge'])
-        remove_keys(st, 'networks', ['edge'])
-        remove_keys(st, 'ports', ['edge'])
-        update_hosts('/etc/hosts', '127.0.0.1 edge.tls.ai')
-
+        multicast(st, edge_ip)
     else:
-        add_entries(st, 'networks', {'prod': {'aliases': ('edge.tls.ai', 'proc-localnode.tls.ai')}}, ['edge'])
-        add_entries(st, 'ports', '4005:4005', ['edge'])
-        remove_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=false', ['edge'])
-        add_entries(st, 'environment', 'ENABLE_OPENSSH_SERVICE=true', ['edge'])
-        remove_entries(st, 'volumes', '/ssd/pipe_data:/root/pipe_data', ['backend', 'collate'])
-        add_entries(st, 'volumes', '/tmp/pipe_data:/root/pipe_data', ['backend', 'collate'])
-        remove_entries(st, 'extra_hosts', 'edge.tls.ai:127.0.0.1', ['edge'])
-        remove_entries(st, 'volumes', '/etc/hosts:/etc/hosts', ['edge'])
-        remove_keys(st, 'dns', ['edge'])
-        remove_keys(st, 'network_mode', ['edge'])
-        remove_entries(st, 'extra_hosts', 'edge.tls.ai:{}'.format(edge_ip), ['backend', 'collate'])
-        remove_keys(st, 'extra_hosts', ['api'])
-        update_hosts('/etc/hosts', '127.0.0.1 edge.tls.ai', False)
+        unicast(st, edge_ip)
 
-    set_mode(mode)
-    print('Mode is now: {}'.format(mode))
     with open(yml_file, 'w') as f:
         yaml.round_trip_dump(st, f, width=int(math.pow(2, 64)), indent=2, block_seq_indent=2)
+    set_mode(mode)
 
 
 main()
